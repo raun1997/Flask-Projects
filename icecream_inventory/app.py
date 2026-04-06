1f736b45bb1c5db18df0b4d7f73ff9098869a3b3
raun1997 <raunakmitra7@gmail.com>
2026-04-06 23:15:08 +0530

    Modified image links and changed Card UI

--- icecream_inventory/app.py
+++ icecream_inventory/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 #             'Baskin Robins Very Strawberry Ice Cup']
 
 icecreams = [
-   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://rukmini1.flixcart.com/image/300/300/xif0q/ice-cream/e/n/q/-original-imahesrbyxaqbcje.jpeg"},
-   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://rukmini1.flixcart.com/image/300/300/xif0q/ice-cream/e/n/q/-original-imahesrbyxaqbcje.jpeg"}
+   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=180/da/cms-assets/cms/product/a5f2160c-815d-4ca0-97f0-a85798dd6e63.png"},
+   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=360/da/cms-assets/cms/product/ddf5f7b6-6be5-4f07-91a7-c2f70ba7f3e4.png"}
    ]
 
 @app.route("/")
