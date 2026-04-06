4d7b82748ef64b35ca83c32e6702e7759759978a
raun1997 <raunakmitra7@gmail.com>
2026-04-06 22:09:57 +0530

    Added more icecreams with image links
    q
    
    git

--- icecream_inventory/app.py
+++ icecream_inventory/app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-icecreams = ['Icecream', 
-            'Vanilla']
+# icecreams = ['Kwality Wall\'s Belgian Chocolate Magnum', 
+#             'Baskin Robins Very Strawberry Ice Cup']
+
+icecreams = [
+   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://rukmini1.flixcart.com/image/300/300/xif0q/ice-cream/e/n/q/-original-imahesrbyxaqbcje.jpeg"},
+   {"name":"Kwality Wall's Belgian Chocolate Magnum", "img":"https://rukmini1.flixcart.com/image/300/300/xif0q/ice-cream/e/n/q/-original-imahesrbyxaqbcje.jpeg"}
+   ]
 
 @app.route("/")
 def index():
